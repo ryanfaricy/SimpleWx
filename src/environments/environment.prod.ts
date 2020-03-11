38be3b1db3edc82ca8ff81b9740718ba50817652
Ryan Faricy <ryan.faricy@communitybrands.com>
2020-03-11 13:42:55 -0400

    updated prod env file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: 'f29d18cd4c974660c3718e23a3e31f10',
+  apiUrl: 'http://api.openweathermap.org/data/2.5',
+  iconUrl: 'http://openweathermap.org/img/wn/',
+  gmapsApiKey: 'AIzaSyC4VyDRQBVSjmuT08FrVoH1cvtyUDDoC1g',
+  units: 'imperial'
 };

2c76db055cd664c58c970823d8a608f36d1b7021
Ryan Faricy <ryan.faricy@communitybrands.com>
2020-03-11 14:07:11 -0400

    add default route to weather

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { WeatherComponent } from './weather/weather.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/weather', pathMatch: 'full' },
   { path: 'weather', component: WeatherComponent }
 ];
 
